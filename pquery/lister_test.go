eb9e2c784895c4ee0363a621eec64ea021c9457c
Damien Whitten <dw@chaos404.com>
2024-01-25 10:30:21 -0800

    External Prototest

--- pquery/lister_test.go
+++ pquery/lister_test.go
@@ -1,67 +1,15 @@
 package pquery
 
 import (
-	"fmt"
-	"io"
 	"strings"
 	"testing"
 
-	"github.com/jhump/protoreflect/desc"
-	"github.com/jhump/protoreflect/desc/protoparse"
+	"github.com/pentops/jsonapi/prototest"
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/protobuf/reflect/protodesc"
-	"google.golang.org/protobuf/reflect/protoreflect"
-	"google.golang.org/protobuf/reflect/protoregistry"
 )
 
-type ResultSet struct {
-	*protoregistry.Files
-}
-
-func (rs ResultSet) MessageByName(t testing.TB, name protoreflect.FullName) protoreflect.MessageDescriptor {
-	t.Helper()
-	md, err := rs.FindDescriptorByName(name)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return md.(protoreflect.MessageDescriptor)
-}
-func DescriptorsFromSource(t testing.TB, sourceFiles map[string]string) ResultSet {
-	t.Helper()
-
-	parser := protoparse.Parser{
-		ImportPaths:           []string{""},
-		IncludeSourceCodeInfo: false,
-		// Load everything which the runtime has already leaded, includes all
-		// the google, buf, psm etc packages, so long as something which uses
-		// them has already beein imported
-		LookupImport: desc.LoadFileDescriptor,
-		Accessor: func(filename string) (io.ReadCloser, error) {
-			if content, ok := sourceFiles[filename]; ok {
-				return io.NopCloser(strings.NewReader(content)), nil
-			}
-			return nil, fmt.Errorf("file not found: %s", filename)
-		},
-	}
-
-	customDesc, err := parser.ParseFiles("test.proto")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	realDesc := desc.ToFileDescriptorSet(customDesc...)
-	descFiles, err := protodesc.NewFiles(realDesc)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return ResultSet{
-		Files: descFiles,
-	}
-}
-
 func TestFieldPath(t *testing.T) {
-	descFiles := DescriptorsFromSource(t, map[string]string{
+	descFiles := prototest.DescriptorsFromSource(t, map[string]string{
 		"test.proto": `
 		syntax = "proto3";
 
@@ -156,7 +104,7 @@ func (c composed) toString() string {
 func TestBuildListReflection(t *testing.T) {
 
 	build := func(t testing.TB, input string, options listerOptions) (*ListReflectionSet, error) {
-		pdf := DescriptorsFromSource(t, map[string]string{
+		pdf := prototest.DescriptorsFromSource(t, map[string]string{
 			"test.proto": `
 				syntax = "proto3";
 
