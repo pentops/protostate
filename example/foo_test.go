6cd6ff115710fe2bb1973ac26ea5fbe5e4842143
Daniel Hess <dan9186@gmail.com>
2024-01-29 16:10:42 -0800

    add test for listing by tenant id and not by tenant id

--- example/foo_test.go
+++ example/foo_test.go
@@ -1375,6 +1375,333 @@ func TestFooDynamicSorting(t *testing.T) {
 	}
 }
 
+func TestFooSortingWithAuth(t *testing.T) {
+	conn := pgtest.GetTestDB(t, pgtest.WithDir("../testproto/db"))
+	db, err := sqrlx.New(conn, sq.Dollar)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	sm, err := NewFooStateMachine(db)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	ss := flowtest.NewStepper[*testing.T]("TestFooSortingWithAuth")
+	defer ss.RunSteps(t)
+
+	queryer, err := testpb.NewFooPSMQuerySet(
+		testpb.DefaultFooPSMQuerySpec(sm.StateTableSpec()),
+		psm.StateQueryOptions{
+			Auth: pquery.AuthProviderFunc(func(ctx context.Context) (map[string]interface{}, error) {
+				authMap := map[string]interface{}{}
+
+				token, err := TokenFromCtx(ctx)
+				if err != nil {
+					return nil, err
+				}
+
+				if token.tenantID != "" {
+					authMap["tenant_id"] = token.tenantID
+				}
+
+				return authMap, nil
+			}),
+		},
+	)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	tenantID1 := uuid.NewString()
+	tenantID2 := uuid.NewString()
+
+	ss.StepC("Create", func(ctx context.Context, a flowtest.Asserter) {
+		tenants := []string{tenantID1, tenantID2}
+
+		for ti := range tenants {
+			tkn := &token{
+				tenantID: tenants[ti],
+			}
+			ctx = tkn.WithToken(ctx)
+
+			restore := silenceLogger()
+			defer restore()
+
+			for ii := 0; ii < 10; ii++ {
+				tt := time.Now()
+				fooID := uuid.NewString()
+
+				event1 := &testpb.FooEvent{
+					Metadata: &testpb.Metadata{
+						EventId:   uuid.NewString(),
+						Timestamp: timestamppb.New(tt),
+						Actor: &testpb.Actor{
+							ActorId: uuid.NewString(),
+						},
+					},
+					TenantId: &tenants[ti],
+					FooId:    fooID,
+					Event: &testpb.FooEventType{
+						Type: &testpb.FooEventType_Created_{
+							Created: &testpb.FooEventType_Created{
+								Name:   "foo",
+								Field:  fmt.Sprintf("foo %d at %s (weighted %d, height %d, length %d)", ii, tt.Format(time.RFC3339Nano), (10+ii)*(ti+1), (50-ii)*(ti+1), (ii%2)*(ti+1)),
+								Weight: ptr.To((10 + int64(ii)) * (int64(ti) + 1)),
+								Height: ptr.To((50 - int64(ii)) * (int64(ti) + 1)),
+								Length: ptr.To((int64(ii%2) * (int64(ti) + 1))),
+							},
+						},
+					},
+				}
+				stateOut, err := sm.Transition(ctx, event1)
+				if err != nil {
+					t.Fatal(err.Error())
+				}
+				a.Equal(testpb.FooStatus_ACTIVE, stateOut.Status)
+				a.Equal(tenants[ti], *stateOut.TenantId)
+			}
+		}
+	})
+
+	// No tenant ID specified, should return all
+	{
+		nextToken := ""
+		ss.StepC("List Page 1", func(ctx context.Context, t flowtest.Asserter) {
+			tkn := &token{
+				tenantID: "",
+			}
+			ctx = tkn.WithToken(ctx)
+
+			req := &testpb.ListFoosRequest{
+				Page: &psml_pb.PageRequest{
+					PageSize: proto.Int64(5),
+				},
+				Query: &psml_pb.QueryRequest{
+					Sort: []*psml_pb.Sort{
+						{
+							Field: "characteristics.weight",
+						},
+					},
+				},
+			}
+			res := &testpb.ListFoosResponse{}
+
+			err = queryer.List(ctx, db, req, res)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			if len(res.Foos) != int(5) {
+				t.Fatalf("expected %d states, got %d", 5, len(res.Foos))
+			}
+
+			for ii, state := range res.Foos {
+				t.Logf("%d: %s", ii, state.Field)
+			}
+
+			for ii, state := range res.Foos {
+				if state.Characteristics.Weight != int64(10+ii) {
+					t.Fatalf("expected weight %d, got %d", 10+ii, state.Characteristics.Weight)
+				}
+			}
+
+			pageResp := res.Page
+
+			if pageResp.GetNextToken() == "" {
+				t.Fatalf("NextToken should not be empty")
+			}
+			if pageResp.NextToken == nil {
+				t.Fatalf("Should not be the final page")
+			}
+
+			nextToken = pageResp.GetNextToken()
+		})
+
+		ss.StepC("List Page 2", func(ctx context.Context, t flowtest.Asserter) {
+			tkn := &token{
+				tenantID: "",
+			}
+			ctx = tkn.WithToken(ctx)
+
+			req := &testpb.ListFoosRequest{
+				Page: &psml_pb.PageRequest{
+					PageSize: proto.Int64(5),
+					Token:    &nextToken,
+				},
+				Query: &psml_pb.QueryRequest{
+					Sort: []*psml_pb.Sort{
+						{
+							Field: "characteristics.weight",
+						},
+					},
+				},
+			}
+			res := &testpb.ListFoosResponse{}
+
+			err = queryer.List(ctx, db, req, res)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			if len(res.Foos) != int(5) {
+				t.Fatalf("expected %d states, got %d", 5, len(res.Foos))
+			}
+
+			for ii, state := range res.Foos {
+				t.Logf("%d: %s", ii, state.Field)
+			}
+
+			for ii, state := range res.Foos {
+				if state.Characteristics.Weight != int64(15+ii) {
+					t.Fatalf("expected weight %d, got %d", 15+ii, state.Characteristics.Weight)
+				}
+			}
+
+			pageResp := res.Page
+
+			if pageResp.GetNextToken() == "" {
+				t.Fatalf("NextToken should not be empty")
+			}
+			if pageResp.NextToken == nil {
+				t.Fatalf("Should not be the final page")
+			}
+		})
+	}
+
+	// Tenant ID specified, should return only that tenant's states
+	{
+		nextToken := ""
+		ss.StepC("List Page 1", func(ctx context.Context, t flowtest.Asserter) {
+			tkn := &token{
+				tenantID: tenantID1,
+			}
+			ctx = tkn.WithToken(ctx)
+
+			req := &testpb.ListFoosRequest{
+				Page: &psml_pb.PageRequest{
+					PageSize: proto.Int64(5),
+				},
+				Query: &psml_pb.QueryRequest{
+					Sort: []*psml_pb.Sort{
+						{
+							Field: "characteristics.weight",
+						},
+					},
+				},
+			}
+			res := &testpb.ListFoosResponse{}
+
+			err = queryer.List(ctx, db, req, res)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			if len(res.Foos) != int(5) {
+				t.Fatalf("expected %d states, got %d", 5, len(res.Foos))
+			}
+
+			for ii, state := range res.Foos {
+				t.Logf("%d: %s", ii, state.Field)
+			}
+
+			for ii, state := range res.Foos {
+				if state.Characteristics.Weight != int64(10+ii) {
+					t.Fatalf("expected weight %d, got %d", 10+ii, state.Characteristics.Weight)
+				}
+
+				if *state.TenantId != tenantID1 {
+					t.Fatalf("expected tenant ID %s, got %s", tenantID1, state.TenantId)
+				}
+			}
+
+			pageResp := res.Page
+
+			if pageResp.GetNextToken() == "" {
+				t.Fatalf("NextToken should not be empty")
+			}
+			if pageResp.NextToken == nil {
+				t.Fatalf("Should not be the final page")
+			}
+
+			nextToken = pageResp.GetNextToken()
+		})
+
+		ss.StepC("List Page 2", func(ctx context.Context, t flowtest.Asserter) {
+			tkn := &token{
+				tenantID: tenantID1,
+			}
+			ctx = tkn.WithToken(ctx)
+
+			req := &testpb.ListFoosRequest{
+				Page: &psml_pb.PageRequest{
+					PageSize: proto.Int64(5),
+					Token:    &nextToken,
+				},
+				Query: &psml_pb.QueryRequest{
+					Sort: []*psml_pb.Sort{
+						{
+							Field: "characteristics.weight",
+						},
+					},
+				},
+			}
+			res := &testpb.ListFoosResponse{}
+
+			err = queryer.List(ctx, db, req, res)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			if len(res.Foos) != int(5) {
+				t.Fatalf("expected %d states, got %d", 5, len(res.Foos))
+			}
+
+			for ii, state := range res.Foos {
+				t.Logf("%d: %s", ii, state.Field)
+			}
+
+			for ii, state := range res.Foos {
+				if state.Characteristics.Weight != int64(15+ii) {
+					t.Fatalf("expected weight %d, got %d", 15+ii, state.Characteristics.Weight)
+				}
+
+				if *state.TenantId != tenantID1 {
+					t.Fatalf("expected tenant ID %s, got %s", tenantID1, state.TenantId)
+				}
+			}
+
+			pageResp := res.Page
+
+			if pageResp.GetNextToken() != "" {
+				t.Fatalf("NextToken should be empty")
+			}
+			if pageResp.NextToken != nil {
+				t.Fatalf("Should be the final page")
+			}
+		})
+	}
+}
+
+type tokenCtxKey struct{}
+
+type token struct {
+	tenantID string
+}
+
+func (t *token) WithToken(ctx context.Context) context.Context {
+	return context.WithValue(ctx, tokenCtxKey{}, t)
+}
+
+func TokenFromCtx(ctx context.Context) (*token, error) {
+	token, exists := ctx.Value(tokenCtxKey{}).(*token)
+	if !exists {
+		return nil, fmt.Errorf("no token found")
+	}
+
+	return token, nil
+}
+
 func silenceLogger() func() {
 	defaultLogger := log.DefaultLogger
 	log.DefaultLogger = log.NewCallbackLogger(func(level string, msg string, fields map[string]interface{}) {
