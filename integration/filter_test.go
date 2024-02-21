338666c2d2bcef664843e384b5386cde947415ad
Daniel Hess <dan9186@gmail.com>
2024-02-21 09:36:16 -0800

    add test for dynamic filter

--- integration/filter_test.go
+++ integration/filter_test.go
@@ -90,3 +90,77 @@ func TestDefaultFiltering(t *testing.T) {
 		})
 	})
 }
+
+func TestDynamicFiltering(t *testing.T) {
+	conn := pgtest.GetTestDB(t, pgtest.WithDir("../testproto/db"))
+	db, err := sqrlx.New(conn, sq.Dollar)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	sm, err := NewFooStateMachine(db, uuid.NewString())
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	ss := flowtest.NewStepper[*testing.T]("TestDynamicFiltering")
+	defer ss.RunSteps(t)
+
+	queryer, err := testpb.NewFooPSMQuerySet(testpb.DefaultFooPSMQuerySpec(sm.StateTableSpec()), psm.StateQueryOptions{})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	tenants := []string{uuid.NewString()}
+	setupFooListableData(t, ss, sm, tenants, 30)
+
+	ss.StepC("List Page", func(ctx context.Context, t flowtest.Asserter) {
+		req := &testpb.ListFoosRequest{
+			Page: &psml_pb.PageRequest{
+				PageSize: proto.Int64(5),
+			},
+			Query: &psml_pb.QueryRequest{
+				Filters: []*psml_pb.Filter{
+					{
+						Field: "characteristics.weight",
+						Type: &psml_pb.Filter_Range{
+							Range: &psml_pb.Range{
+								Min: "12",
+								Max: "15",
+							},
+						},
+					},
+				},
+			},
+		}
+		res := &testpb.ListFoosResponse{}
+
+		err = queryer.List(ctx, db, req, res)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+
+		if len(res.Foos) != int(4) {
+			t.Fatalf("expected %d states, got %d", 4, len(res.Foos))
+		}
+
+		for ii, state := range res.Foos {
+			t.Logf("%d: %s", ii, state.Field)
+		}
+
+		for ii, state := range res.Foos {
+			if state.Characteristics.Weight != int64(12+ii) {
+				t.Fatalf("expected weight %d, got %d", 12+ii, state.Characteristics.Weight)
+			}
+		}
+
+		pageResp := res.Page
+
+		if pageResp.GetNextToken() != "" {
+			t.Fatalf("NextToken should be empty")
+		}
+		if pageResp.NextToken != nil {
+			t.Fatalf("Should be the final page")
+		}
+	})
+}
